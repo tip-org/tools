854b2a399991c291e9480ead6ebaf76dc11727a2
Dan Rosén <danr@chalmers.se>
2015-03-31 15:57:57 +0200

    Use any Data.Map for GHC 7.4

--- tip-lib/src/Tip/AddCase.hs
+++ tip-lib/src/Tip/AddCase.hs
@@ -6,8 +6,8 @@ module Tip.AddCase where
 
 import Tip
 import Tip.Fresh
-import qualified Data.Map.Strict as Map
-import Data.Map.Strict(Map)
+import qualified Data.Map as Map
+import Data.Map(Map)
 
 addCase :: Name a => Theory a -> Fresh (Theory a)
 addCase thy =
