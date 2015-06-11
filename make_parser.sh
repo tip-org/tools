b1f5f592583329336e03fd6be3c1eb3354bfb64f
Dan Rosén <danr@chalmers.se>
2015-06-11 10:12:53 +0200

    Fix for cabal (build-tools and extra-source-files)

--- make_parser.sh
+++ make_parser.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd tip-lib/src
 bnfc -p Tip.Parser TIP.bnfc
-happy -gca Tip/Parser/ParTIP.y
-alex -g Tip/Parser/LexTIP.x
+# happy -gca Tip/Parser/ParTIP.y
+# alex -g Tip/Parser/LexTIP.x
