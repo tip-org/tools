fc3606539035ea3e678f823d95c29b32349b63ac
Dan Rosén <danr@chalmers.se>
2015-10-10 13:51:34 +0200

    Simplify tip-spec api

--- tip-spec/executable/Main.hs
+++ tip-spec/executable/Main.hs
@@ -7,17 +7,6 @@ import Tip.Parser (parse)
 
 import qualified Tip.Pretty.SMT as SMT
 
-import QuickSpec (choppyQuickSpec)
-
-import qualified QuickSpec.Signature as QS
-
-import Tip.Core
-import Tip.Fresh
-import Tip.Utils
-import Data.List
-import GHC.IO.Handle
-import System.IO
-
 main :: IO ()
 main =
   do args <- getArgs
@@ -26,23 +15,11 @@ main =
         f:es   -> handle es =<< readFile f
         es     -> handle es =<< getContents
 
-toStderr :: IO a -> IO a
-toStderr mx = do
-  oldStdout <- hDuplicate stdout
-  hDuplicateTo stderr stdout
-  x <- mx
-  hDuplicateTo oldStdout stdout
-  hClose oldStdout
-  return x
-
 handle :: [String] -> String -> IO ()
 handle es s =
   case parse s of
     Left err  -> error $ "Parse failed: " ++ err
     Right thy ->
-      do ((chops,sig),rm) <- theorySignature thy
-         sig' <- toStderr $ choppyQuickSpec chops sig
-         let bm  = backMap thy rm
-         let fms = mapM (trProperty bm) (nub (QS.background sig')) `freshFrom` thy
-         print (SMT.ppTheory (thy { thy_asserts = thy_asserts thy ++ fms }))
+      do thy' <- exploreTheory thy
+         print (SMT.ppTheory thy')
 
--- tip-spec/src/Tip/QuickSpec.hs
+++ tip-spec/src/Tip/QuickSpec.hs
@@ -7,12 +7,20 @@ import Tip.Fresh (Name)
 import Tip.Haskell.Translate
 import Tip.Core (Theory)
 
-import QuickSpec (Signature, Constant)
+import qualified QuickSpec.Signature as QS
+import QuickSpec (Signature, Constant, choppyQuickSpec)
 
 import Language.Haskell.Interpreter
 import System.FilePath
 import System.Directory
 import System.IO.Temp
+
+import Tip.Core hiding (Signature)
+import Tip.Fresh
+import Tip.Utils
+import Data.List
+import GHC.IO.Handle
+import System.IO
 
 type ChoppedSignature = ([(Constant,[Int])],Signature)
 
@@ -22,7 +30,7 @@ theorySignature thy =
   withSystemTempDirectory "tip-spec" $
     \ dir ->
       do let a_file = dir </> "A" <.> "hs"
-         let (thy_doc, rename_map) = ppTheoryWithRenamings QuickSpec thy
+         let (thy_doc, rename_map) = ppTheoryWithRenamings "A" QuickSpec thy
          writeFile a_file (show thy_doc)
          setCurrentDirectory dir
          runInterpreter $
@@ -30,4 +38,21 @@ theorySignature thy =
               setImports ["A","QuickSpec.Signature","QuickSpec.Term","Prelude"]
               sig <- interpret "sig" (undefined :: ChoppedSignature)
               return (sig,rename_map)
+
+exploreTheory :: Name a => Theory a -> IO (Theory a)
+exploreTheory thy =
+  do ((chops,sig),rm) <- theorySignature thy
+     sig' <- toStderr $ choppyQuickSpec chops sig
+     let bm  = backMap thy rm
+     let fms = mapM (trProperty bm) (nub (QS.background sig')) `freshFrom` thy
+     return thy { thy_asserts = thy_asserts thy ++ fms }
+
+toStderr :: IO a -> IO a
+toStderr mx = do
+  oldStdout <- hDuplicate stdout
+  hDuplicateTo stderr stdout
+  x <- mx
+  hDuplicateTo oldStdout stdout
+  hClose oldStdout
+  return x
 
