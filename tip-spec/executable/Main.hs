5c7f8cb3cd82a1dc71db7fd7c9e2378e0bd5a5cd
Nick Smallbone <nick.smallbone@gmail.com>
2016-06-29 15:14:15 +0200

    A disgraceful hack to set GHC_PACKAGE_PATH under stack

--- tip-spec/executable/Main.hs
+++ tip-spec/executable/Main.hs
@@ -1,19 +1,32 @@
+{-# LANGUAGE CPP #-}
+#ifdef STACK
+{-# LANGUAGE TemplateHaskell #-}
+#endif
 module Main where
 
 import System.Environment
 
 import Tip.QuickSpec
 import Tip.Parser (parse)
+#ifdef STACK
+import Language.Haskell.TH.Syntax
+import System.Process
+#endif
 
 import qualified Tip.Pretty.SMT as SMT
 
 main :: IO ()
-main =
-  do args <- getArgs
-     case args of
-        "-":es -> handle es =<< getContents
-        f:es   -> handle es =<< readFile f
-        es     -> handle es =<< getContents
+main = do
+#ifdef STACK
+  let pkgdb = $(qRunIO (readProcess "stack" ["exec", "--", "sh", "-c", "echo $GHC_PACKAGE_PATH"] "") >>= lift)
+
+  setEnv "GHC_PACKAGE_PATH" (head (lines pkgdb))
+#endif
+  args <- getArgs
+  case args of
+     "-":es -> handle es =<< getContents
+     f:es   -> handle es =<< readFile f
+     es     -> handle es =<< getContents
 
 handle :: [String] -> String -> IO ()
 handle es s =
