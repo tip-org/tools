2cad76a82fee77485f095ecbc54b0eb2caeefaf5
Nick Smallbone <nick@smallbone.se>
2023-09-28 14:25:22 +0200

    Simplify dropAttributes.

--- tip-lib/src/Tip/Pass/DropAttributes.hs
+++ tip-lib/src/Tip/Pass/DropAttributes.hs
@@ -2,8 +2,8 @@ module Tip.Pass.DropAttributes where
 
 import Tip.Core
 
-dropAttributes :: Theory a -> Theory a
-dropAttributes thy =
+filterAttributes :: (String -> Bool) -> Theory a -> Theory a
+filterAttributes p thy =
   thy {
     thy_datatypes =
       [ clear dt { data_cons = map clear (data_cons dt) }
@@ -14,18 +14,10 @@ dropAttributes thy =
     thy_asserts = map clear (thy_asserts thy) }
   where
     clear :: HasAttr a => a -> a
-    clear = putAttrs []
+    clear x = putAttrs [attr | attr <- getAttrs x, p (fst attr)] x
+
+dropAttributes :: Theory a -> Theory a
+dropAttributes thy = filterAttributes (const False) thy
 
 dropAttribute :: String -> Theory a -> Theory a
-dropAttribute name thy =
-  thy {
-    thy_datatypes =
-      [ clear dt { data_cons = map clear (data_cons dt) }
-      | dt <- thy_datatypes thy ],
-    thy_sorts = map clear (thy_sorts thy),
-    thy_sigs = map clear (thy_sigs thy),
-    thy_funcs = map clear (thy_funcs thy),
-    thy_asserts = map clear (thy_asserts thy) }
-  where
-    clear :: HasAttr a => a -> a
-    clear = delAttr (unitAttr name)
+dropAttribute name thy = filterAttributes (/= name) thy
